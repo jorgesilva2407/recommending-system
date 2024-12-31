e8e6bb7913ab360f7b9984e2fb5270b9212e1aad
jorgesilva2407 <jorge.lima2407@gmail.com>
2024-12-31 18:53:52 -0300

    download file from github

--- model/main.py
+++ model/main.py
@@ -1,18 +1,28 @@
 import os
 import pickle
 import pandas as pd
+import requests
 from model import Model
 
 
 def main():
-    input_filename = os.getenv("INPUT_FILENAME")
-    print("INPUT_FILENAME", input_filename)
+    input_file_link = os.getenv("INPUT_FILE_LINK")
+    print("INPUT_FILE_LINK", input_file_link)
     output_filename = os.getenv("OUTPUT_FILENAME")
     print("OUTPUT_FILENAME", output_filename)
     min_support = float(os.getenv("MIN_SUPPORT"))
     print("MIN_SUPPORT", min_support)
     min_confidence = float(os.getenv("MIN_CONFIDENCE"))
     print("MIN_CONFIDENCE", min_confidence)
+
+    input_filename = "/tmp/playlists.csv"
+    response = requests.get(input_file_link)
+
+    if response.status_code != 200:
+        raise Exception(f"Failed to download data from {input_file_link}")
+
+    with open(input_filename, "wb") as f:
+        f.write(response.content)
 
     df = pd.read_csv(input_filename)
     print("Data loaded")
